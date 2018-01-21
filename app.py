2daf0d323713b89cf01d5a56a2295fcf23c0aa1e
denskif <denskif.91.91@gmail.com>
2018-01-22 00:30:00 +0200

    Sandy working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 
 app = Flask(__name__)
@@ -8,4 +10,6 @@ app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
 
-
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Configuration(object):
 
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:80931207152@localhost/test1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:80931207152@localhost/test2'
