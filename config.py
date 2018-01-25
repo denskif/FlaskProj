6b30eaf54ce3d14aee5c2a0492aee57f46a6d913
denskif <denskif.91.91@gmail.com>
2018-01-25 21:57:45 +0200

    Wenthdy adding

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Configuration(object):
 
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:80931207152@localhost/test2'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:80931207152@localhost/test3'
