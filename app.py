3467e981544a42f210540cad4674e362ea017408
denskif <denskif.91.91@gmail.com>
2018-01-29 21:53:12 +0200

    Monday fack work

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from config import Configuration
+
 from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+
+from flask_admin import Admin
+from flask_admin.contrib.sqlamodel import ModelView
 
 
 app = Flask(__name__)
@@ -13,3 +18,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+# ADMIN
+
+from models import *
+
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
