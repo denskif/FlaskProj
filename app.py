9a2f087d29ea6216e2d85e56f8c68a4a12003e85
denskif <denskif.91.91@gmail.com>
2018-01-31 21:40:24 +0200

    Wenthdy working

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from flask_script import Manager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
+from flask_security import SQLAlchemyUserDatastore, Security
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -26,3 +28,8 @@ from models import *
 admin = Admin(app)
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Tag, db.session))
+
+# Security
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
--- config.py
+++ config.py
@@ -2,5 +2,11 @@ class Configuration(object):
 
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:80931207152@localhost/test3'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:80931207152@localhost/test4'
     SECRET_KEY = 'something very secret'
+
+    # Security
+
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
+
